6e1900271503a6d08f0682ed12a4ee266b09adc1
Mahesh Theril <maheshtheril@live.com>
2025-11-23 05:29:46 +0530

    Cleanup: Stop tracking dist/ and ensure countries route is committed

--- dist/services/mailer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendVerifyEmail = sendVerifyEmail;
-// server/src/services/mailer.ts
-async function sendVerifyEmail(opts) {
-    // TODO: plug your provider (Nodemailer/Resend/SES)
-    console.log("[mailer] sendVerifyEmail →", opts);
-    return { ok: true };
-}
--- dist/services/seeder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.enqueueSeedJob = enqueueSeedJob;
-async function enqueueSeedJob(job) {
-    // TODO: integrate BullMQ/queue later.
-    console.log("[seeder] enqueueSeedJob →", job);
-    return { queued: true };
-}
